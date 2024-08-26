e7a3fedd130eb4463781cda2863a33dbc7f42714
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2024-08-27 00:30:14 +0200

    tailwind installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomepageHeroImage from './components/HomepageHeroImage'
+import Chatbot from './components/Chatbot'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-r from-sky-400 to-indigo-400 h-screen">
+        <HomepageHeroImage/>
+        <Chatbot/>
     </div>
   );
 }
---
+++ src/components/HomepageHeroImage.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+
+const MyComponent: FC = ({}) => {
+ 
+  return (
+    <div className="flex flex-col justify-center items-center py-16">
+     <h1 className="text-5xl">
+        Krokad🐟
+     </h1>
+
+    </div>
+  );
+};
+
+export default MyComponent;
