b83438b006a09987afa766dabc2a24fcb74f2f65
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2024-08-29 19:02:22 +0200

    Test

--- src/components/HomepageHeroImage.tsx
+++ src/components/HomepageHeroImage.tsx
@@ -5,7 +5,7 @@ const MyComponent: FC = ({}) => {
  
   return (
     <div className="flex justify-center items-center py-6">
-     <h1 className="text-5xl">
+     <h1 className="text-4xl">
         <p className='text-4xl'>Lorem</p>
      </h1>
 
