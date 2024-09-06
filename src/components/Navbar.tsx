fdff60a7ba3f46960c383213b38b117572067252
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2024-09-06 17:30:19 +0200

    zenTokyoZoo  font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const MyComponent: FC = ({}) => {
   return (
     <div className="flex justify-center items-center">
      <h1 className="text-4xl mt-7">
-        <p className='text-6xl font-zenTokyoZoo'>Krokad</p>
+        <p className='text-7xl font-zenTokyoZoo'>krokad</p>
      </h1>
     </div>
   );
