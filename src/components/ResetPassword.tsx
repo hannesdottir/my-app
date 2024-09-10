289ae0d6fac6b70397235bd75e045452c78f977e
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2024-09-10 21:09:04 +0200

    .

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import ChangeLanguage from "../components/ChangeLanguage"
 
 const ResetPassword: FC = ({}) => {
   
